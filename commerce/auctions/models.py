f1957a0f6cce205345d21bf1dc0109e37a415227
sippinyotrippin <van.didenko@gmail.com>
2023-08-28 11:54:18 +0300

    add User, AuctionListings, Bids, Comments models

---
+++ commerce/auctions/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class AuctionListings(models.Model):
+    pass
+
+
+class Bids(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
