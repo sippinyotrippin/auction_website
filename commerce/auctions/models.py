36d57ce4b350b1f4c84adaea56ece10f51e5c74e
sippinyotrippin <van.didenko@gmail.com>
2023-08-28 14:24:52 +0300

    added all models to admin.py to manipulate them using django admin interface; take some changes in models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,8 +6,23 @@ class User(AbstractUser):
     pass
 
 
-class AuctionListing(models.Model):
-    pass
+class Category(models.Model):
+    category_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=200)
+    current_price = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, related_name="category")
+
+    def __str__(self):
+        return self.title
 
 
 class Bid(models.Model):
