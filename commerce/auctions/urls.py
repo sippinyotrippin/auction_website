06b49020a7781f21e943a1b4679ffdf961d022e2
sippinyotrippin <van.didenko@gmail.com>
2023-08-30 11:07:04 +0300

    Create 'Watchlist' view

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create_listing/", views.create, name="create"),
-    path("listings/<int:listing_id>", views.listing_page, name="page")
+    path("listings/<int:listing_id>", views.listing_page, name="page"),
+    path("watchlist", views.watchlist, name="watchlist")
 ]
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -95,3 +95,7 @@ def listing_page(request, listing_id):
     return render(request, "auctions/listing_page.html", {
         "item": listing
     })
+
+
+def watchlist(request):
+    return render(request, "auctions/watchlist.html")
